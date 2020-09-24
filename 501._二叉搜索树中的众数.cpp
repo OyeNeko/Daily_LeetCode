8fde38d8929f37a9f26ed0eb49df1c3624190dda
Night_Cat <44079174+OyeNeko@users.noreply.github.com>
2020-09-24 20:29:20 +0800

    Update 501._二叉搜索树中的众数.cpp

--- 501._二叉搜索树中的众数.cpp
+++ 501._二叉搜索树中的众数.cpp
@@ -37,15 +37,15 @@ public:
                 continue;
             }
             pre = cur->left;//遍历左结点
-            while (pre->right && pre->right != cur) {//遍历当前结点的左节点的最右结点，得到中序遍历的前驱
+            while (pre->right && pre->right != cur) {//遍历当前结点的左节点的最右结点，如果为空则没有链接，如果与cur相同则说明已经链接了。
                 pre = pre->right;
             }
-            if (!pre->right) {
+            if (!pre->right) {//没有链接就说明还没有遍历到。
                 pre->right = cur;
                 cur = cur->left;
-            } else {
+            } else {//已经链接了则已遍历，需要将其去掉
                 pre->right = nullptr;
-                update(cur->val);
+                update(cur->val);//传入值，记录频率
                 cur = cur->right;
             }
         }
