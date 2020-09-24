9870bfe78e369b247a122ff1262ead47ad9b181c
Night_Cat <44079174+OyeNeko@users.noreply.github.com>
2020-09-24 20:25:50 +0800

    Create 501._二叉搜索树中的众数.cpp
    C++，可以用中序遍历，因为中序遍历后相同的值是连续存在的，所以可以用一个变量来记录当前值的频率，用一个数组来记录相同频率的值，然后可以根据中序遍历的顺序获得结点的前驱结点，减少堆栈的空间开销。

---
+++ 501._二叉搜索树中的众数.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int base, count, maxCount;
+    vector<int> answer;
+
+    void update(int x) {
+        if (x == base) {
+            ++count;
+        } else {
+            count = 1;
+            base = x;
+        }
+        if (count == maxCount) {
+            answer.push_back(base);
+        }
+        if (count > maxCount) {
+            maxCount = count;
+            answer = vector<int> {base};
+        }
+    }
+
+    vector<int> findMode(TreeNode* root) {
+        TreeNode *cur = root, *pre = nullptr;
+        while (cur) {
+            if (!cur->left) {//判断左结点是否为空
+                update(cur->val);//若为空则直接遍历右结点
+                cur = cur->right;
+                continue;
+            }
+            pre = cur->left;//遍历左结点
+            while (pre->right && pre->right != cur) {//遍历当前结点的左节点的最右结点，得到中序遍历的前驱
+                pre = pre->right;
+            }
+            if (!pre->right) {
+                pre->right = cur;
+                cur = cur->left;
+            } else {
+                pre->right = nullptr;
+                update(cur->val);
+                cur = cur->right;
+            }
+        }
+        return answer;
+    }
+};
