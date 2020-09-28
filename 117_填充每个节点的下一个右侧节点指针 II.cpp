5ad7d7ae49c14a4d7445ef17bf1712144d5cb312
Night_Cat <44079174+OyeNeko@users.noreply.github.com>
2020-09-28 23:20:53 +0800

    Create 117_填充每个节点的下一个右侧节点指针 II.cpp

---
+++ 117_填充每个节点的下一个右侧节点指针 II.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if (!root) {
+            return nullptr;
+        }
+        queue<Node*> q;
+        q.push(root);
+        while (!q.empty()) {
+            int n = q.size();
+            Node *last = nullptr;
+            for (int i = 1; i <= n; ++i) {
+                Node *f = q.front();
+                q.pop();
+                if (f->left) {
+                    q.push(f->left);
+                }
+                if (f->right) {
+                    q.push(f->right);
+                }
+                if (i != 1) {
+                    last->next = f;
+                }
+                last = f;
+            }
+        }
+        return root;
+    }
+};
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/populating-next-right-pointers-in-each-node-ii/solution/tian-chong-mei-ge-jie-dian-de-xia-yi-ge-you-ce-15/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
